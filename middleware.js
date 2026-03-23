256f07d475baf4c409f8d470b00bd7881cefc0be
nikostreet-ux <niko.street@gmail.com>
2026-03-23 01:00:54 -0300

    Add Basic Auth middleware

---
+++ middleware.js
@@ -0,0 +1,25 @@
+export const config = {
+  matcher: ['/((?!api/|favicon.ico|_next/static|_next/image).*)'],
+};
+
+export default function middleware(request) {
+  const authorizationHeader = request.headers.get('authorization');
+
+  if (authorizationHeader) {
+    const basicAuth = authorizationHeader.split(' ')[1];
+    const [user, password] = atob(basicAuth).split(':');
+
+    if (user === 'admin' && password === 'mallplaza2026') {
+      return new Response(null, {
+        headers: { 'x-middleware-next': '1' }
+      });
+    }
+  }
+
+  return new Response('Autenticación requerida', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Acceso Seguro Dashboard Usabilidad"',
+    },
+  });
+}
